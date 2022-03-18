f0fc543b59a77ccdd8d7686d58a12263721f387d
Tran Nguyen <58058227+dexter-nguyen@users.noreply.github.com>
2022-03-18 09:59:01 -0700

    Create score.cs

---
+++ Vive Stable/Assets/score.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    public static int Score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnGUI()
+    {
+        GUI.Box(new Rect(50, 50, 200, 200), "Score: " + Score.ToString());
+
+    }
+}
