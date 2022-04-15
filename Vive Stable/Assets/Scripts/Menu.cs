46e2be94c2f10f6272f7c0786094045eb30db3eb
Kenneth Shuto <kennethshuto@gmail.com>
2022-04-15 14:36:37 -0700

    4/15/22 update

--- Vive Stable/Assets/Scripts/Menu.cs
+++ Vive Stable/Assets/Scripts/Menu.cs
@@ -6,11 +6,23 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public Text scoreText;
+    public Transform net;
+    private Vector3 startNetPos;
+
+    void Start()
+    {
+        startNetPos = net.transform.position;
+    }
 
     public void testButton()
     {
         score.Score += 1000;
         scoreText.text = "Score: " + score.Score;
         Debug.Log("Button Clicked");
+    }
+
+    public void resetNet()
+    {
+        net.position = startNetPos;
     }
 }
